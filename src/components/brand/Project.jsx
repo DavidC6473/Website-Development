7cee07cac8a8f6c1e8589f01c98ef491b8b61ca1
David <dcc6473@gmail.com>
2023-06-30 16:52:18 +0100

    Added modals and responsivity

--- src/components/brand/Project.jsx
+++ src/components/brand/Project.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 import './project.css';
 import { Link } from 'react-router-dom';
 
+import skillsData from './skills.json';
+
+Modal.setAppElement('#root'); // Add this line to define the app element
+
 const Project = () => {
-  const skills = [
-    { name: 'JS', logo: 'devicon-javascript-plain' },
-    { name: 'TS', logo: 'devicon-typescript-plain' },
-    { name: 'React', logo: 'devicon-react-original' },
-    { name: 'Python', logo: 'devicon-python-plain' },
-    { name: 'NodeJS', logo: 'devicon-nodejs-plain' },
-    { name: 'ExpressJS', logo: 'devicon-express-original' },
-    { name: 'Swift', logo: 'devicon-swift-plain' },
-    { name: 'Git', logo: 'devicon-git-plain' },
-  ];
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedSkill, setSelectedSkill] = useState(null);
+
+  const openModal = (skill) => {
+    setSelectedSkill(skill);
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
 
   return (
     <div className='dc__projects' id='projects'>
@@ -22,36 +28,69 @@ const Project = () => {
         </div>
         <div className='project1'>
           <p className='dc__projects-animation'>
-            <a className='a_p1' href="/vrjam">VR Jam</a>
+            <a className='a_p1' href='/vrjam'>
+              VR Jam
+            </a>
           </p>
         </div>
         <div className='project2'>
           <p className='dc__projects-animation'>
-            <a className='a_p2' href="/nbaapp">NBA App</a>
+            <a className='a_p2' href='/nbaapp'>
+              NBA App
+            </a>
           </p>
         </div>
         <div className='project3'>
           <p className='dc__projects-animation'>
-            <a className='a_p4' href="/java">Art-Palette</a>
+            <a className='a_p4' href='/java'>
+              Art-Palette
+            </a>
           </p>
         </div>
         <div className='project4'>
           <p className='dc__projects-animation'>
-            <a className='a_p3' href="/turtle">Fractals</a>
+            <a className='a_p3' href='/turtle'>
+              Fractals
+            </a>
           </p>
         </div>
         <div className='dc__projects-title'>
           <p className='dc-projects-title-animation'>SKILLS</p>
         </div>
         <div className='dc__skills-grid'>
-          {skills.map((skill, index) => (
-            <div key={index} className='dc__skill'>
+          {skillsData.map((skill, index) => (
+            <div
+              key={index}
+              className='dc__skill'
+              onClick={() => openModal(skill)}
+            >
               <i className={`devicon ${skill.logo} skill-logo`}></i>
               <p className='dc__skill-name'>{skill.name}</p>
             </div>
           ))}
         </div>
       </div>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel='Skill Modal'
+        className='modal'
+        overlayClassName='modal-overlay'
+        ariaHideApp={false} // Set ariaHideApp to false
+      >
+        {selectedSkill && (
+          <div>
+            <i className={`devicon ${selectedSkill.logo} modal-logo`}></i>
+            <h2 className='modal-skill-name'>{selectedSkill.name}</h2>
+            <p className='modal-text'>{selectedSkill.paragraph}</p>
+            <a className='modal-link' href={selectedSkill.link} target='_blank'>Learn more</a>
+          </div>
+        )}
+        <button className='modal-close-button' onClick={closeModal}>
+          X
+        </button>
+      </Modal>
     </div>
   );
 };
