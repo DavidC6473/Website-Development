7fdfc714381d0509355337d23fd3bd87612af777
David <dcc6473@gmail.com>
2023-07-05 21:08:42 +0100

    fixed merge conflict

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,24 +1,71 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './footer.css';
 
-const Footer = () => {
-    return (
-        <div className='dc__footer'>
-            <div className='dc__footer-content' id='contact'>
-                <div className='dc__footer-title'>
-                    <p class='dc__footer-title-animation'>Get In Touch</p>
-                </div>
-                <div className='dc__footer-contactform'>
-                    <form class="dc__contactform" /*action='contactform.php' method='post'*/>
-                        <input type="text" class='form-name' name='name' placeholder='Your Name'></input>
-                        <input type="text" name='email' placeholder='Your Email'></input>
-                        <textarea name='message' placeholder='Message'></textarea>
-                        <button className='send-button' type='submit' name='submit'>SEND</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    )
-}
+const sendEmail = (event, formRef) => {
+  event.preventDefault();
 
-export default Footer
+  const name = event.target.elements.name.value;
+  const email = event.target.elements.email.value;
+  const message = event.target.elements.message.value;
+
+  const data = {
+    sender: {
+      name: 'david-clarke.com',
+      email: 'd_cc@live.ie',
+    },
+    to: [
+      {
+        email: 'dcc6473@gmail.com',
+      },
+    ],
+    subject: 'New Contact Form Submission',
+    htmlContent: `<html><head></head><body><p>Name: ${name}</p><p>Email: ${email}</p><p>Message: ${message}</p></body></html>`,
+  };
+
+  fetch('https://api.brevo.com/v3/smtp/email', {
+    method: 'POST',
+    headers: {
+      'accept': 'application/json',
+      'api-key': 'Brevo-API-Key',
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      if (response.ok) {
+        console.log('Email sent!');
+        alert('Email sent!');
+
+        // Reset form fields
+        formRef.current.reset();
+      } else {
+        throw new Error('Failed to send email.');
+      }
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      alert('Email not sent, please try again');
+    });
+};
+
+const Footer = () => {
+  const formRef = useRef(null);
+
+  return (
+    <div className='dc__footer'>
+      <div className='dc__footer-content' id='contact'>
+        <div className='dc__footer-title'>
+          <p className='dc__footer-title-animation'>Get In Touch</p>
+        </div>
+        <div className='dc__footer-contactform'>
+          <form className="dc__contactform" ref={formRef} onSubmit={(event) => sendEmail(event, formRef)}>
+            <input type="text" className='form-name' name='name' placeholder='Your Name' />
+            <input type="email" name='email' placeholder='Your Email' />
+            <textarea name='message' placeholder='Message'></textarea>
+            <button className='send-button' type='submit' name='submit'>SEND</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
