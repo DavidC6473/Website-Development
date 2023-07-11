8ec257e155b1e052afec186b691128e203027e06
David <dcc6473@gmail.com>
2023-07-11 11:45:49 +0100

    Updated folder names

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as Article } from './article/Article';
-export { default as Project } from './brand/Project';
+export { default as Project } from './project/Project';
 export { default as CTA } from './cta/CTA';
 export { default as ChatBot } from './chatbot/ChatBot';
 export { default as Navbar } from './navbar/Navbar';
