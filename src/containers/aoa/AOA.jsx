6a71d3aeaf58eb735754b71eb09d2782bb287dd6
David <dcc6473@gmail.com>
2023-07-12 23:50:55 +0100

    Added 404 page and updated ReadMe

---
+++ src/containers/aoa/AOA.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './aoa.css';
+
+const AOA = () => {
+    return (
+        <div className='aoa-container'>
+            <div className='aoa-content'>
+                <h1 className='404'>404</h1>
+                <h2 className='Sorry'>I'm sorry, the page you're looking for doesn't exist.</h2>
+            </div>
+        </div>
+    )
+}
+
+export default AOA
