e6d61843d9a9a6fc61322093a1d9a6fda6a16ca7
David <dcc6473@gmail.com>
2023-07-14 11:22:14 +0100

    Added scroll prompt and functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import { Footer, Header, Features } from './containers';
+import { Footer, Header, Features, Scroll } from './containers';
 import { VRJam, NBAApp, Turtle, Java, Other, Resume, AOA } from './containers';
 import { Project, Navbar, ChatBot } from './components';
 import './App.css';
@@ -17,6 +17,7 @@ const App = () => {
             <Features />
             <ChatBot />
             <Project />
+            <Scroll />
           </>} />
           <Route path="/vrjam" element={<VRJam />} />
           <Route path="/nbaapp" element={<NBAApp />} />
--- src/containers/scroll/Scroll.jsx
+++ src/containers/scroll/Scroll.jsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './scroll.css';
 
 const Scroll = () => {
-    return (
-        <div id="loader-wrapper">
-            <div id="loader"></div>
+  const [scrollDirection, setScrollDirection] = useState('down');
 
-            <div class="loader-section section-left"></div>
-            <div class="loader-section section-right"></div>
-        </div>
-    )
-}
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrolled = window.scrollY || document.documentElement.scrollTop;
 
-export default Scroll
+      // Check if user scrolled down or up
+      if (scrolled > 0) {
+        setScrollDirection('up');
+      } else {
+        setScrollDirection('down');
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  const scrollButtonContent = scrollDirection === 'up' ? 'TOP' : 'SCROLL';
+  const scrollButtonArrowClass = scrollDirection === 'up' ? 'scroll-to-top' : '';
+
+  return (
+    <div className={`scroll-button ${scrollButtonArrowClass}`} onClick={scrollToTop}>
+      <span>{scrollButtonContent}</span>
+      <span>&#129147;</span>
+    </div>
+  );
+};
+
+export default Scroll;
